953e41ef9da2d9fa25ce0523702a2799af7fc865
janhviiiiii <janhvibaraskar03@gmail.com>
2024-12-04 00:33:29 +0530

    new code

---
+++ REACT/vite-project/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h2>Home</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem molestiae repellat autem! Veniam blanditiis consequatur porro, nostrum vero temporibus facilis nesciunt est placeat. Debitis rerum incidunt ipsa ducimus neque autem.</p>
+    </div>
+  )
+}
+
+export default Home
